630b22ff8d9840e48dc4674fa815757c2b59adf6
Joey Lopez <joeyl509625@gmail.com>
2021-02-02 00:47:03 -0600

    Added useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { Navbar} from 'react-bootstrap';
+import {useState, useEffect} from 'react'
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+  const [load, setLoad ] = useState(false)
+  const [error , setError] = useState('')
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts/3')
+     .then(res => {
+       setPosts(res.data);
+       setLoad(true);
+     })
+     .catch(err => {
+       setError(error.message);
+       setLoad(true)
+     })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Home'>
+      <Navbar bg='dark' variant='light'>
+        <Navbar.Brand style={{color: 'white'}}>
+      NavBar for Single-Page-App  
+        </Navbar.Brand>
+      </Navbar> 
+      <div className="main">
+        <section className='content'>
+          {/* <div>content: {posts.map((post, index) => <p key={index}>{post.body}</p> )} </div> */}
+          <div>
+            <p>{posts.body}</p>
+           
+          </div>
+         
+        </section>
+      </div>    
     </div>
   );
 }
